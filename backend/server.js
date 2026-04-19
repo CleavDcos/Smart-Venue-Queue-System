e17b35fe64f718309b0dacbe6e13d83c360bb512
CleavDcos <138748716+CleavDcos@users.noreply.github.com>
2026-04-19 13:13:22 +0530

    i

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,11 @@ if (process.env.NODE_ENV !== 'test') {
 }
 
 // ─── Middleware ─────────────────────────────────────────────────────────────
+
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
-  credentials: true,
+  origin: "*",
 }));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
